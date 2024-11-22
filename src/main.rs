df2a3bab2cb77754408147b8e205b06c29da1043
brad-wilkes <rbwilkes@yahoo.com>
2024-11-21 21:19:59 -0600

    inject new sort mod

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::fs::File;
 use std::io;
 use std::io::BufRead;
 use std::path::Path;
+use sortstuff::Sortstuff;
 
 fn main() {
     let file_path = "data.txt"; // where is the file stored?
@@ -25,48 +26,7 @@ fn main() {
             }
         }
 
-        Ok(bubble_sort(numbers))
+        Ok(Sortstuff::bubble_sort(numbers))
     }
-    fn bubble_sort(mut arr: Vec<i32>) -> Vec<i32> {
-        let n = arr.len();
-        let mut start = 0;
-        let mut end = n - 1;
 
-        while start < end {
-            let mut swapped = false;
-
-            // Forward pass
-            for i in start..end {
-                if arr[i] > arr[i + 1] {
-                    arr.swap(i, i + 1);
-                    swapped = true;
-                }
-            }
-
-            // If no swaps in forward pass, the array is sorted
-            if !swapped {
-                break;
-            }
-
-            // Backward pass
-            swapped = false;
-            for i in (start..end).rev() {
-                if arr[i] > arr[i + 1] {
-                    arr.swap(i, i + 1);
-                    swapped = true;
-                }
-            }
-
-            // If no swaps in backward pass, the array is sorted
-            if !swapped {
-                break;
-            }
-
-            // Optimizations
-            start += 1;
-            end -= 1;
-        }
-
-        arr
-    }
 }
